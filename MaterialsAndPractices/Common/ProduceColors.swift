5253e30ab78d2612b359cf69bc00ee0487d94c77
Jeff Kunzelman <jeff@river.io>
2025-08-25 11:39:51 -0500

    theme updates

---
+++ MaterialsAndPractices/Common/ProduceColors.swift
@@ -0,0 +1,154 @@
+// Auto-generated from ProduceColors.xcassets
+import SwiftUI
+#if canImport(UIKit)
+import UIKit
+#endif
+
+public enum ProduceColorsCatalog {
+    public static let all: [String] = [
+        "AlliumAmericanflag170200140",
+        "AlliumYellowYellowglobe24522060",
+        "AmaranthBurgundy1202040",
+        "AmaranthCallalooamaranth12017090",
+        "AmaranthGoldenGoldengiant23519070",
+        "AmaranthGreenGreencallaloo9016090",
+        "AmaranthOrangeChinesegiantorange24013040",
+        "AmaranthRedAllredleaf2003030",
+        "AmaranthRedRedgarnet1502035",
+        "AmaranthWhiteWhiteleaf240240235",
+        "Bean0863per1407050",
+        "BeanBlackBlackbear353535",
+        "BeanBlueBluelake70120220",
+        "BeanEnvy190160110",
+        "BeetchardChioggia140200120",
+        "BeetchardRedRubyred1802040",
+        "BrassicaJadecross225205180",
+        "BrassicaPurpleAmericanpurpletop12060160",
+        "Cantaloupe242190114",
+        "CarrotChantenay23714533",
+        "CornCreamHoneyandcream245235205",
+        "CornSilverqueen25022070",
+        "CucumbermelonBostonpickling235210160",
+        "Honeydew200235190",
+        "LettuceButtercrunch110180100",
+        "PeaBerken1508560",
+        "PeaDesi120190100",
+        "SpinachChinesemulticolorspinach6012070",
+        "SquashpumpkinBigmax24711830",
+        "SweetpotatoBeauregard18113680",
+        "TomatoDusky2224122",
+        "TomatoGoldYukongold23018060",
+        "Watermelon2345460",
+    ]
+
+    public enum Name {
+        public static let alliumamericanflag170200140: String = "AlliumAmericanflag170200140"
+        public static let alliumyellowyellowglobe24522060: String = "AlliumYellowYellowglobe24522060"
+        public static let amaranthburgundy1202040: String = "AmaranthBurgundy1202040"
+        public static let amaranthcallalooamaranth12017090: String = "AmaranthCallalooamaranth12017090"
+        public static let amaranthgoldengoldengiant23519070: String = "AmaranthGoldenGoldengiant23519070"
+        public static let amaranthgreengreencallaloo9016090: String = "AmaranthGreenGreencallaloo9016090"
+        public static let amaranthorangechinesegiantorange24013040: String = "AmaranthOrangeChinesegiantorange24013040"
+        public static let amaranthredallredleaf2003030: String = "AmaranthRedAllredleaf2003030"
+        public static let amaranthredredgarnet1502035: String = "AmaranthRedRedgarnet1502035"
+        public static let amaranthwhitewhiteleaf240240235: String = "AmaranthWhiteWhiteleaf240240235"
+        public static let bean0863per1407050: String = "Bean0863per1407050"
+        public static let beanblackblackbear353535: String = "BeanBlackBlackbear353535"
+        public static let beanbluebluelake70120220: String = "BeanBlueBluelake70120220"
+        public static let beanenvy190160110: String = "BeanEnvy190160110"
+        public static let beetchardchioggia140200120: String = "BeetchardChioggia140200120"
+        public static let beetchardredrubyred1802040: String = "BeetchardRedRubyred1802040"
+        public static let brassicajadecross225205180: String = "BrassicaJadecross225205180"
+        public static let brassicapurpleamericanpurpletop12060160: String = "BrassicaPurpleAmericanpurpletop12060160"
+        public static let cantaloupe242190114: String = "Cantaloupe242190114"
+        public static let carrotchantenay23714533: String = "CarrotChantenay23714533"
+        public static let corncreamhoneyandcream245235205: String = "CornCreamHoneyandcream245235205"
+        public static let cornsilverqueen25022070: String = "CornSilverqueen25022070"
+        public static let cucumbermelonbostonpickling235210160: String = "CucumbermelonBostonpickling235210160"
+        public static let honeydew200235190: String = "Honeydew200235190"
+        public static let lettucebuttercrunch110180100: String = "LettuceButtercrunch110180100"
+        public static let peaberken1508560: String = "PeaBerken1508560"
+        public static let peadesi120190100: String = "PeaDesi120190100"
+        public static let spinachchinesemulticolorspinach6012070: String = "SpinachChinesemulticolorspinach6012070"
+        public static let squashpumpkinbigmax24711830: String = "SquashpumpkinBigmax24711830"
+        public static let sweetpotatobeauregard18113680: String = "SweetpotatoBeauregard18113680"
+        public static let tomatodusky2224122: String = "TomatoDusky2224122"
+        public static let tomatogoldyukongold23018060: String = "TomatoGoldYukongold23018060"
+        public static let watermelon2345460: String = "Watermelon2345460"
+    }
+}
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public extension Color {
+    static var alliumamericanflag170200140: Color { Color("AlliumAmericanflag170200140") }
+    static var alliumyellowyellowglobe24522060: Color { Color("AlliumYellowYellowglobe24522060") }
+    static var amaranthburgundy1202040: Color { Color("AmaranthBurgundy1202040") }
+    static var amaranthcallalooamaranth12017090: Color { Color("AmaranthCallalooamaranth12017090") }
+    static var amaranthgoldengoldengiant23519070: Color { Color("AmaranthGoldenGoldengiant23519070") }
+    static var amaranthgreengreencallaloo9016090: Color { Color("AmaranthGreenGreencallaloo9016090") }
+    static var amaranthorangechinesegiantorange24013040: Color { Color("AmaranthOrangeChinesegiantorange24013040") }
+    static var amaranthredallredleaf2003030: Color { Color("AmaranthRedAllredleaf2003030") }
+    static var amaranthredredgarnet1502035: Color { Color("AmaranthRedRedgarnet1502035") }
+    static var amaranthwhitewhiteleaf240240235: Color { Color("AmaranthWhiteWhiteleaf240240235") }
+    static var bean0863per1407050: Color { Color("Bean0863per1407050") }
+    static var beanblackblackbear353535: Color { Color("BeanBlackBlackbear353535") }
+    static var beanbluebluelake70120220: Color { Color("BeanBlueBluelake70120220") }
+    static var beanenvy190160110: Color { Color("BeanEnvy190160110") }
+    static var beetchardchioggia140200120: Color { Color("BeetchardChioggia140200120") }
+    static var beetchardredrubyred1802040: Color { Color("BeetchardRedRubyred1802040") }
+    static var brassicajadecross225205180: Color { Color("BrassicaJadecross225205180") }
+    static var brassicapurpleamericanpurpletop12060160: Color { Color("BrassicaPurpleAmericanpurpletop12060160") }
+    static var cantaloupe242190114: Color { Color("Cantaloupe242190114") }
+    static var carrotchantenay23714533: Color { Color("CarrotChantenay23714533") }
+    static var corncreamhoneyandcream245235205: Color { Color("CornCreamHoneyandcream245235205") }
+    static var cornsilverqueen25022070: Color { Color("CornSilverqueen25022070") }
+    static var cucumbermelonbostonpickling235210160: Color { Color("CucumbermelonBostonpickling235210160") }
+    static var honeydew200235190: Color { Color("Honeydew200235190") }
+    static var lettucebuttercrunch110180100: Color { Color("LettuceButtercrunch110180100") }
+    static var peaberken1508560: Color { Color("PeaBerken1508560") }
+    static var peadesi120190100: Color { Color("PeaDesi120190100") }
+    static var spinachchinesemulticolorspinach6012070: Color { Color("SpinachChinesemulticolorspinach6012070") }
+    static var squashpumpkinbigmax24711830: Color { Color("SquashpumpkinBigmax24711830") }
+    static var sweetpotatobeauregard18113680: Color { Color("SweetpotatoBeauregard18113680") }
+    static var tomatodusky2224122: Color { Color("TomatoDusky2224122") }
+    static var tomatogoldyukongold23018060: Color { Color("TomatoGoldYukongold23018060") }
+    static var watermelon2345460: Color { Color("Watermelon2345460") }
+}
+
+#if canImport(UIKit)
+public extension UIColor {
+    static var alliumamericanflag170200140: UIColor { UIColor(named: "AlliumAmericanflag170200140")! }
+    static var alliumyellowyellowglobe24522060: UIColor { UIColor(named: "AlliumYellowYellowglobe24522060")! }
+    static var amaranthburgundy1202040: UIColor { UIColor(named: "AmaranthBurgundy1202040")! }
+    static var amaranthcallalooamaranth12017090: UIColor { UIColor(named: "AmaranthCallalooamaranth12017090")! }
+    static var amaranthgoldengoldengiant23519070: UIColor { UIColor(named: "AmaranthGoldenGoldengiant23519070")! }
+    static var amaranthgreengreencallaloo9016090: UIColor { UIColor(named: "AmaranthGreenGreencallaloo9016090")! }
+    static var amaranthorangechinesegiantorange24013040: UIColor { UIColor(named: "AmaranthOrangeChinesegiantorange24013040")! }
+    static var amaranthredallredleaf2003030: UIColor { UIColor(named: "AmaranthRedAllredleaf2003030")! }
+    static var amaranthredredgarnet1502035: UIColor { UIColor(named: "AmaranthRedRedgarnet1502035")! }
+    static var amaranthwhitewhiteleaf240240235: UIColor { UIColor(named: "AmaranthWhiteWhiteleaf240240235")! }
+    static var bean0863per1407050: UIColor { UIColor(named: "Bean0863per1407050")! }
+    static var beanblackblackbear353535: UIColor { UIColor(named: "BeanBlackBlackbear353535")! }
+    static var beanbluebluelake70120220: UIColor { UIColor(named: "BeanBlueBluelake70120220")! }
+    static var beanenvy190160110: UIColor { UIColor(named: "BeanEnvy190160110")! }
+    static var beetchardchioggia140200120: UIColor { UIColor(named: "BeetchardChioggia140200120")! }
+    static var beetchardredrubyred1802040: UIColor { UIColor(named: "BeetchardRedRubyred1802040")! }
+    static var brassicajadecross225205180: UIColor { UIColor(named: "BrassicaJadecross225205180")! }
+    static var brassicapurpleamericanpurpletop12060160: UIColor { UIColor(named: "BrassicaPurpleAmericanpurpletop12060160")! }
+    static var cantaloupe242190114: UIColor { UIColor(named: "Cantaloupe242190114")! }
+    static var carrotchantenay23714533: UIColor { UIColor(named: "CarrotChantenay23714533")! }
+    static var corncreamhoneyandcream245235205: UIColor { UIColor(named: "CornCreamHoneyandcream245235205")! }
+    static var cornsilverqueen25022070: UIColor { UIColor(named: "CornSilverqueen25022070")! }
+    static var cucumbermelonbostonpickling235210160: UIColor { UIColor(named: "CucumbermelonBostonpickling235210160")! }
+    static var honeydew200235190: UIColor { UIColor(named: "Honeydew200235190")! }
+    static var lettucebuttercrunch110180100: UIColor { UIColor(named: "LettuceButtercrunch110180100")! }
+    static var peaberken1508560: UIColor { UIColor(named: "PeaBerken1508560")! }
+    static var peadesi120190100: UIColor { UIColor(named: "PeaDesi120190100")! }
+    static var spinachchinesemulticolorspinach6012070: UIColor { UIColor(named: "SpinachChinesemulticolorspinach6012070")! }
+    static var squashpumpkinbigmax24711830: UIColor { UIColor(named: "SquashpumpkinBigmax24711830")! }
+    static var sweetpotatobeauregard18113680: UIColor { UIColor(named: "SweetpotatoBeauregard18113680")! }
+    static var tomatodusky2224122: UIColor { UIColor(named: "TomatoDusky2224122")! }
+    static var tomatogoldyukongold23018060: UIColor { UIColor(named: "TomatoGoldYukongold23018060")! }
+    static var watermelon2345460: UIColor { UIColor(named: "Watermelon2345460")! }
+}
+#endif
